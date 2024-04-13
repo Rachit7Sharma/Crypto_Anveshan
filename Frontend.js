cb0b5c1bf9288759a538940b705a630ff175fa45
Rachit7Sharma <140009651+Rachit7Sharma@users.noreply.github.com>
2024-04-13 09:42:12 +0530

    Update Frontend.js

--- Frontend.js
+++ Frontend.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Blockchain Wallet Dashboard</h1>
+        <h1>Crypto Anveshan</h1>
       </header>
       <main>
         {}
